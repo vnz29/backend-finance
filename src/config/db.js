8f55d7e57b0ac10b6d85a138fb1604b7a904a30f
vnz29 <vince.great29@gmail.com>
2025-05-01 21:08:59 +0800

    mongo uri setup

--- src/config/db.js
+++ src/config/db.js
@@ -7,10 +7,7 @@ const mongoURI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB Atlas");
   } catch (err) {
     console.error("Failed to connect to MongoDB Atlas:", err);
