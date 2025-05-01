a123ae9940ce8e21cbff00787f508a7d42f84707
vnz29 <vince.great29@gmail.com>
2025-05-01 20:59:20 +0800

    config mongodb

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,19 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoURI = process.env.MONGO_URI 
+const mongoURI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
-    try {
-      await mongoose.connect(mongoURI);
-      console.log('Connected to MongoDB Atlas');
-    } catch (err) {
-      console.error('Failed to connect to MongoDB Atlas:', err);
-      process.exit(1); // Exit the application in case of error
-    }
-  };
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB Atlas:", err);
+    process.exit(1); // Exit the application in case of error
+  }
+};
