b8d5f706414fe7d4017bad8c8ee506216badddb0
vnz29 <vince.great29@gmail.com>
2025-05-09 17:49:39 +0800

    add cors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import express from "express";
-import { connectDB } from './config/db.js'
-import routes from './routes/indexRoutes.js'
+import { connectDB } from "./config/db.js";
+import routes from "./routes/indexRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
+// Allow frontend origin
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // if you're using cookies or authorization headers
+  })
+);
 // Connect to MongoDB
 connectDB();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(routes)
+app.use(express.json());
+app.use(cookieParser());
+app.use(routes);
 
-app.listen(PORT, () =>{
-    console.log(`RUNNING IN PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`RUNNING IN PORT ${PORT}`);
+});
