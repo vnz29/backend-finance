080ed93196114df8214f833409197f0fbf00a419
vnz29 <vince.great29@gmail.com>
2025-05-11 13:59:28 +0800

    new updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -68,7 +68,7 @@ export const loginUser = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false,
-      sameSite: "Strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
